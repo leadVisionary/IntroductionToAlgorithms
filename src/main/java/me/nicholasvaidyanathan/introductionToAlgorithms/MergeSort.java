97a7ef0512c1bc34f818177ea68f8903cd081bfe
Nick Vaidyanathan <visionary.software.solutions@gmail.com>
2018-04-07 13:37:22 -0700

    added merge without sentinels.

--- src/main/java/me/nicholasvaidyanathan/introductionToAlgorithms/MergeSort.java
+++ src/main/java/me/nicholasvaidyanathan/introductionToAlgorithms/MergeSort.java
@@ -10,7 +10,7 @@ public class MergeSort {
             final int midpoint  = (startIndex + endIndex) / 2;
             mergeSort(array, startIndex, midpoint);
             mergeSort(array, midpoint+1, endIndex);
-            merge(array, startIndex, midpoint, endIndex);
+            mergeWithoutSentinels(array, startIndex, midpoint, endIndex);
         }
     }
 
@@ -39,6 +39,44 @@ public class MergeSort {
                 array[k] = right[j];
                 j = j + 1;
             }
+        }
+    }
+
+    private static void mergeWithoutSentinels(int[] array, int p, int q, int r) {
+        if (p > q || p > r || q > r) {
+            throw new IllegalStateException("p <= q < r");
+        }
+        final int n1 = q - p + 1;
+        final int n2 = r - q;
+        final int[] left = new int[n1];
+        final int[] right = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            left[i] = array[p+ i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = array[q+j+1];
+        }
+        int i = 0 ,j = 0, l = p;
+        while (i < n1 && j < n2){
+            if (left[i] <= right[j]) {
+                array[l] = left[i];
+                i = i + 1;
+            } else {
+                array[l] = right[j];
+                j = j + 1;
+            }
+            l = l + 1;
+        }
+        while (i < n1) {
+            array[l] = left[i];
+            i = i + 1;
+            l = l + 1;
+        }
+
+        while (j < n2) {
+            array[l] = right[j];
+            j = j + 1;
+            l = l + 1;
         }
     }
 }
