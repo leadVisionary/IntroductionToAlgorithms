c9d767c4230ec8c5ccedeb32e6d3130e1eefe3fa
Nick Vaidyanathan <visionary.software.solutions@gmail.com>
2018-04-07 13:06:56 -0700

    merge sort.

---
+++ src/test/java/me/nicholasvaidyanathan/introductionToAlgorithms/MergeSortTest.java
@@ -0,0 +1,22 @@
+package me.nicholasvaidyanathan.introductionToAlgorithms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MergeSortTest {
+    @Test
+    public void figure_2_4() {
+        final int[] numbers = new int[] { 5, 2, 4, 7, 1, 3, 2, 6};
+        MergeSort.sort(numbers);
+        final int[] expected = new int[] { 1, 2, 2, 3, 4, 5, 6, 7};
+        Assert.assertArrayEquals(expected, numbers);
+    }
+
+    @Test
+    public void figure_2_3() {
+        final int[] numbers = new int[] { 2, 4, 5, 7, 1, 2, 3, 6};
+        MergeSort.sort(numbers);
+        final int[] expected = new int[] { 1, 2, 2, 3, 4, 5, 6, 7};
+        Assert.assertArrayEquals(expected, numbers);
+    }
+}
